f0b9533dbf4b3b617c275b3b6aa3db6a7d959e70
Jhonatan Diniz <jhonatangomesdiniz@gmail.com>
2024-10-29 17:31:33 -0300

    Criado enum StatusTask

---
+++ src/main/java/com/todo/api/entities/ENUMS/StatusTask.java
@@ -0,0 +1,28 @@
+package com.todo.api.entities.ENUMS;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public enum StatusTask {
+    EM_ANDAMENTO(0, "Em_Andamento"), CONCLUIDO(1, "Concluído"), ATRASADO(2, "Atrasado");
+
+    private Integer code;
+    private String description;
+
+    public static StatusTask toEnum(Integer cod){
+        if(cod == null){
+            return null;
+        }
+
+        for (StatusTask status : StatusTask.values()) {
+            if(cod.equals(status.getCode())){
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Status inválido");
+    }
+}
